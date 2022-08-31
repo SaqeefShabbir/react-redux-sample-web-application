7ca21044dc449c69ddef7d7c3c5e5ccf8daa5823
CONURE-SAQEEF <muhammad.shabbir@conurets.com>
2022-08-31 13:52:24 +0500

    added initial web files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+
+import Login from './Features/Login/Controller';
+import Main from './Features/Main/Controller';
+import AuthRoute from './Features/Auth/AuthRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <AuthRoute path="/" component={Login} exact={true} type="guest" />
+        <AuthRoute path="/Main" component={Main} type="private" />
+      </Switch>
+    </Router>
   );
 }
 
