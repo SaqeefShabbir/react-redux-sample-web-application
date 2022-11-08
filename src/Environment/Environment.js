b06d459f65f5ff768fa4790db5a7bacea5421824
Saqeef Shabbir <62050152+SaqeefShabbir@users.noreply.github.com>
2022-11-08 16:29:00 +0500

    Update Environment.js

--- src/Environment/Environment.js
+++ src/Environment/Environment.js
@@ -1,3 +1,4 @@
 export default class Environment {
-    static apiUrl = "http://localhost:5000/api";
+    //static apiUrl = "http://localhost:5000/api";
+    static apiUrl = "https://mywebapp0001.azurewebsites.net/api";
 }
